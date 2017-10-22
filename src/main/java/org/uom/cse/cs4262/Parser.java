ef5e2937b462cc5bf558933c19aba91cc12e2e5a
ldclakmal <ldclakmal@gmail.com>
2017-10-22 18:58:53 +0530

    Add parser class

---
+++ src/main/java/org/uom/cse/cs4262/Parser.java
@@ -0,0 +1,16 @@
+package org.uom.cse.cs4262;
+
+import org.uom.cse.cs4262.api.message.Message;
+
+/**
+ * @author Chanaka Lakmal
+ * @date 22/10/2017
+ * @since 1.0
+ */
+
+public class Parser {
+
+    public static Message parse(String message) {
+        return null;
+    }
+}
