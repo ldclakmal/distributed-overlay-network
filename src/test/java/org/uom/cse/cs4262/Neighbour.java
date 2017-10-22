4a768426424ed469992e281c12bdad7ca3093661
ldclakmal <ldclakmal@gmail.com>
2017-10-22 13:57:02 +0530

    Setup bootstrap server code

---
+++ src/test/java/org/uom/cse/cs4262/Neighbour.java
@@ -0,0 +1,25 @@
+package org.uom.cse.cs4262;
+
+class Neighbour {
+    private String ip;
+    private int port;
+    private String username;
+
+    public Neighbour(String ip, int port, String username) {
+        this.ip = ip;
+        this.port = port;
+        this.username = username;
+    }
+
+    public String getIp() {
+        return this.ip;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+}
