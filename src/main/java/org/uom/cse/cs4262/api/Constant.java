82dbf521c8665e9695f4d436d89415bad19ad48e
ldclakmal <ldclakmal@gmail.com>
2017-10-22 19:43:34 +0530

    Add constants

---
+++ src/main/java/org/uom/cse/cs4262/api/Constant.java
@@ -0,0 +1,22 @@
+package org.uom.cse.cs4262.api;
+
+/**
+ * @author Chanaka Lakmal
+ * @date 22/10/2017
+ * @since 1.0
+ */
+
+public class Constant {
+
+    public final static String IP_BOOTSTRAP_SERVER = "127.0.0.1";
+    public final static String IP_NODE = "127.0.0.1";
+    public final static int PORT_BOOTSTRAP_SERVER = 55555;
+    public final static int PORT_NODE = 44444;
+
+    public static class Command {
+
+        public final static String REG = "REG";
+        public final static String REGOK = "REGOK";
+    }
+
+}
