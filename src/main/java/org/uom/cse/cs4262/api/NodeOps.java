8f75ab8ade334d581470bb866339de137916b1ce
ldclakmal <ldclakmal@gmail.com>
2017-10-23 19:44:21 +0530

    Merge branch 'master' of https://github.com/ldclakmal/DistributedOverlayNetwork
    # Conflicts:
    #	src/main/java/org/uom/cse/cs4262/NodeOpsUDP.java
    #	src/main/java/org/uom/cse/cs4262/api/NodeOps.java

--- src/main/java/org/uom/cse/cs4262/api/NodeOps.java
+++ src/main/java/org/uom/cse/cs4262/api/NodeOps.java
@@ -1,22 +1,39 @@
 package org.uom.cse.cs4262.api;
 
+import org.uom.cse.cs4262.api.message.Message;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * @author Chanaka Lakmal
  * @date 22/10/2017
  * @since 1.0
  */
 
-public interface NodeOps {
+public abstract class NodeOps {
 
-    void start();
+    public abstract void start();
 
-    void register();
+    public abstract void register();
 
-    void unRegister();
+    public abstract void unRegister();
 
-    void join(Credential credential);
+    public abstract void join(Credential credential);
 
-    void leave();
+    public abstract void leave();
 
-    void search(Credential credential);
+    public abstract void search(Credential credential);
+
+    public ArrayList<String> createFileList() {
+        ArrayList<String> fileList = new ArrayList<>();
+        fileList.add("Twilight");
+        fileList.add("Jack");
+        Collections.shuffle(fileList);
+        return (ArrayList<String>) fileList.subList(0, 5);
+    }
+
+    public abstract void processResponse(Message response);
+
+    public abstract boolean isRegOk();
 }
