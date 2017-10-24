5f16b85c330d89d4e5c7cd9417bd016640ebeecf
chaminw <chamin.13@cse.mrt.ac.lk>
2017-10-24 07:35:22 +0530

    Add error response

---
+++ src/main/java/org/uom/cse/cs4262/api/message/response/ErrorResponse.java
@@ -0,0 +1,15 @@
+package org.uom.cse.cs4262.api.message.response;
+
+import org.uom.cse.cs4262.api.message.Message;
+
+/**
+ * @author Chamin Wickramarathna
+ * @date 24/10/2017
+ * @since 1.0
+ */
+public class ErrorResponse extends Message {
+    @Override
+    public String getMessageAsString(String message) {
+        return super.getMessageAsString(message);
+    }
+}
