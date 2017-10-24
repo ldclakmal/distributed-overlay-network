f78d326ab2990c228d8936e7f4ea5b0e0417f8bd
chaminw <chamin.13@cse.mrt.ac.lk>
2017-10-24 07:58:32 +0530

    Add unregister request

---
+++ src/main/java/org/uom/cse/cs4262/api/message/request/UnregisterRequest.java
@@ -0,0 +1,34 @@
+package org.uom.cse.cs4262.api.message.request;
+
+import org.uom.cse.cs4262.api.Credential;
+import org.uom.cse.cs4262.api.message.Message;
+
+/**
+ * @author Chamin Wickramarathna
+ * @date 22/10/2017
+ * @since 1.0
+ */
+
+public class UnregisterRequest extends Message {
+
+
+    private Credential credential;
+
+    public UnregisterRequest(Credential credential) {
+        this.credential = credential;
+    }
+
+    public Credential getCredential() {
+        return credential;
+    }
+
+    public void setCredential(Credential credential) {
+        this.credential = credential;
+    }
+
+    @Override
+    public String getMessageAsString(String message) {
+        message += " " + this.getCredential().getIp() + " " + this.getCredential().getPort() + " " + this.getCredential().getUsername();
+        return super.getMessageAsString(message);
+    }
+}
