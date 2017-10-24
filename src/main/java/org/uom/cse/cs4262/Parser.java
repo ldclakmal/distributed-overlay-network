cd73cd7058f564ba6755d03b248329342f6fe07a
Chandu Herath <cbherath93@gmail.com>
2017-10-24 11:31:30 +0530

    Add parser test method

--- src/main/java/org/uom/cse/cs4262/Parser.java
+++ src/main/java/org/uom/cse/cs4262/Parser.java
@@ -35,7 +35,6 @@ public class Parser {
                 nodes.add(new Credential(ip, port, null));
             }
             RegisterResponse registerResponse = new RegisterResponse(numOfNodes, nodes);
-            //TODO: split the message and create registerResponse object. Refer BootstrapServer.java class for tokenize
             return registerResponse;
 
         } else if (command.equals(Constant.Command.UNREGOK)) {
@@ -63,8 +62,7 @@ public class Parser {
             return new SearchResponse(numOfFiles, endNodeCredentials, hops, fileList);
 
         } else if (command.equals(Constant.Command.ERROR)) {
-            //TODO handle error response @Chandu
-
+            return new ErrorResponse();
         }
 
         return null;
